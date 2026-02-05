c170e2199fbeaa65557f65a30cff50a6599f3dc8
sammyROG <trojura27@gmail.com>
2026-02-04 23:54:30 -0300

    added world and song placeholders

--- Scripts/Songmixer.cs
+++ Scripts/Songmixer.cs
@@ -40,6 +40,36 @@ public partial class Songmixer : AudioStreamPlayer
 				instance.Play();
 				currentSong = 2;
 				break;
+			case 3:
+				instance.Stream = instance.world3;
+				instance.Play();
+				currentSong = 3;
+				break;
+			case 4:
+				instance.Stream = instance.world4;
+				instance.Play();
+				currentSong = 4;
+				break;
+			case 5:
+				instance.Stream = instance.world5;
+				instance.Play();
+				currentSong = 5;
+				break;
+			case 6:
+				instance.Stream = instance.world6;
+				instance.Play();
+				currentSong = 6;
+				break;
+			case 7:
+				instance.Stream = instance.world7;
+				instance.Play();
+				currentSong = 7;
+				break;
+			case 8:
+				instance.Stream = instance.world8;
+				instance.Play();
+				currentSong = 8;
+				break;
 			case 9:
 				instance.Stream = instance.mainmenu;
 				instance.Play();
