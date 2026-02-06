95fc4a7e39dbdd0d17b135d64707a1a9d0ff9f49
sammyROG <trojura27@gmail.com>
2026-02-06 14:44:53 -0300

    added deinforced sprite and gg bg

--- Scripts/Songmixer.cs
+++ Scripts/Songmixer.cs
@@ -20,7 +20,6 @@ public partial class Songmixer : AudioStreamPlayer
 	public override void _Ready()
 	{
 		instance = this; // idk what this does
-		Console.WriteLine("test");
 	}
 	public static void PlaySong(int songToPlay)
 	{
@@ -32,50 +31,32 @@ public partial class Songmixer : AudioStreamPlayer
 		{
 			case 1:
 				instance.Stream = instance.world1;
-				instance.Play();
-				currentSong = 1;
 				break;
 			case 2:
 				instance.Stream = instance.world2;
-				instance.Play();
-				currentSong = 2;
 				break;
 			case 3:
 				instance.Stream = instance.world3;
-				instance.Play();
-				currentSong = 3;
 				break;
 			case 4:
 				instance.Stream = instance.world4;
-				instance.Play();
-				currentSong = 4;
 				break;
 			case 5:
 				instance.Stream = instance.world5;
-				instance.Play();
-				currentSong = 5;
 				break;
 			case 6:
 				instance.Stream = instance.world6;
-				instance.Play();
-				currentSong = 6;
 				break;
 			case 7:
 				instance.Stream = instance.world7;
-				instance.Play();
-				currentSong = 7;
 				break;
 			case 8:
 				instance.Stream = instance.world8;
-				instance.Play();
-				currentSong = 8;
 				break;
 			case 9:
 				instance.Stream = instance.mainmenu;
-				instance.Play();
-				currentSong = 9;
 				break;
-				
 		}
+		currentSong = songToPlay;
 	}
 }
